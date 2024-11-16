1b8d87359f6af06d437a578339281077ddc11834
mridulhossain <mridulcse8@gmail.com>
2024-11-16 23:06:42 +0600

    added a static logg class for logging, here a text file created which will use to write logg

---
+++ src/LoggingDemoApplication/LoggingDemo/Data/LoggingDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LoggingDemo.Models;
+
+namespace LoggingDemo.Data
+{
+    public class LoggingDemoContext : DbContext
+    {
+        public LoggingDemoContext (DbContextOptions<LoggingDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LoggingDemo.Models.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ src/LoggingDemoApplication/LoggingDemo/LoggEvents.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+namespace LoggingDemo
+{
+    public static class LoggEvents
+    {
+        public static void LogToFile(string title,string message)
+        {
+            StreamWriter swlog;
+            string fileName = "LogFile.txt";
+
+            if (!File.Exists(fileName)) 
+            { 
+                swlog = new StreamWriter(fileName);
+            }
+            else
+            {
+                swlog = File.AppendText(fileName);
+            }
+
+            swlog.WriteLine("log entry");
+            swlog.WriteLine("{0}{1}",DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString());
+            swlog.Close();
+        }
+    }
+}
